9db06110fd7b25d12088945dff57fcdd396aeb94
plumberg <hana.ryzhuk@gmail.com>
2018-11-04 15:22:37 -0500

    Added OnClick and icon launcher

--- app/src/main/java/com/example/hana/realhw1app/MainActivity.java
+++ app/src/main/java/com/example/hana/realhw1app/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.hana.realhw1app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText input;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void onClick(View v) {
+        String r = null;
+
+        // Проверяем поля на пустоту
+        if (TextUtils.isEmpty(input.getText().toString()))
+                {
+            return;
+        }
+
+        if (R.id.button == v.getId()){
+            r = input.getText().toString();
+        }
+
+        // формируем строку вывода
+        result.setText(r);
     }
 }
