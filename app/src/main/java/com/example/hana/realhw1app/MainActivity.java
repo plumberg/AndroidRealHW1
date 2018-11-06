34bfea809370327e5a705c45fac38a0f5912c472
plumberg <hana.ryzhuk@gmail.com>
2018-11-06 15:33:12 -0500

    onClick changed, padding added

--- app/src/main/java/com/example/hana/realhw1app/MainActivity.java
+++ app/src/main/java/com/example/hana/realhw1app/MainActivity.java
@@ -4,38 +4,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.view.View.OnClickListener;
 public class MainActivity extends AppCompatActivity {
     EditText input;
+    Button okB;
     TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        input = (EditText) this.findViewById(R.id.editText);
+        okB = (Button) this.findViewById(R.id.button);
 
-
-    public void onClick(View v) {
-        String r = null;
-
-        if (R.id.button == v.getId()){
-            // Проверяем поля на пустоту
-            if (TextUtils.isEmpty(input.getText().toString()))
+        okB.setOnClickListener (new View.OnClickListener ()
+        {
+            @Override
+            public void onClick (View view)
             {
-                Toast.makeText(MainActivity.this, "Empty input", Toast.LENGTH_LONG);
-                return;
-            }
-            else {
-                r = input.getText().toString();
-            }
-        }
+                String r = "";
+                Toast.makeText(MainActivity.this, "Button clicked", Toast.LENGTH_LONG);
+                if (R.id.button == view.getId()){
+                    // Проверяем поля на пустоту
+                    if (TextUtils.isEmpty(input.getText().toString()))
+                    {
+                        Toast.makeText(MainActivity.this, "Empty input", Toast.LENGTH_LONG);
+                        return;
+                    }
+                    else {
+                        r = input.getText().toString();
+                    }
+                }
 
-        // формируем строку вывода
-        result.setText(r);
+                // формируем строку вывода
+                result.setText(r);
+            }
+        });
     }
+
+
+
 }
